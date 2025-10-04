c57ad304afea612e978e6c3210a01d424cf41766
chko0 <chadi.kouzayha@gmail.com>
2025-10-04 13:32:17 +0300

    refactor: Update menu items data

--- src/data/menuData.js
+++ src/data/menuData.js
@@ -7,19 +7,20 @@ const menuData = {
       {
         name: "Espresso",
         price: 3,
-        description: "Strong & rich, made from finely ground beans",
+        description:
+          "Strong, rich shot made from finely ground specialty beans",
         image: "espresso.jpg",
       },
       {
         name: "Americano",
         price: 3.5,
-        description: "Espresso diluted with hot water for a smoother taste",
+        description: "Bold espresso softened with hot water",
         image: "americano.jpg",
       },
       {
         name: "Cappuccino",
         price: 4,
-        description: "Espresso topped with steamed milk foam",
+        description: "Espresso topped with steamed milk & velvety foam",
         image: "cappuccino.jpg",
       },
       {
@@ -31,18 +32,17 @@ const menuData = {
       },
       {
         name: "Almond Latte",
-        price: 4.5,
+        price: 5,
+        description: "Almond milk latte with rich espresso flavor",
+        image: "almond_latte.jpg",
         popular: true,
-        new: false,
         vegan: true,
         lactoseFree: true,
-        image: "almond_latte.jpg",
-        description: "Almond milk latte with rich espresso flavor",
       },
       {
         name: "Mocha",
         price: 5,
-        description: "Espresso mixed with chocolate & steamed milk",
+        description: "Espresso mixed with premium chocolate & steamed milk",
         image: "mocha.jpg",
       },
     ],
@@ -54,47 +54,35 @@ const menuData = {
       {
         name: "Croissant",
         price: 2.5,
-        description: "Flaky & buttery French pastry",
+        description: "Classic flaky & buttery French pastry",
         image: "croissant.jpg",
-      },
-      {
-        name: "Muffin",
-        price: 2,
-        description: "Soft baked treat with chocolate chips",
-        image: "muffin.jpg",
-        popular: true,
-      },
-      {
-        name: "Cinnamon Roll",
-        price: 3,
-        description: "Sweet roll filled with cinnamon & topped with icing",
-        image: "cinnamon_roll.jpg",
-      },
-      {
-        name: "Cheesecake Slice",
-        price: 4,
-        description: "Rich & creamy classic cheesecake",
-        image: "cheesecake.jpg",
-        popular: true,
-      },
-      {
-        name: "Almond Croissant",
-        price: 3,
-        description: "Flaky croissant with almond cream filling",
-        image: "almond_croissant.jpg",
-        vegan: true,
-      },
-      {
-        name: "Strawberry Croissant",
-        price: 4,
-        description: "Buttery croissant filled with fresh strawberries",
-        image: "strawberry_croissant.jpg",
       },
       {
         name: "Chocolate Croissant",
         price: 3,
-        description: "Buttery croissant filled with rich chocolate",
+        description: "Buttery pastry filled with rich dark chocolate",
         image: "chocolate_croissant.jpg",
+      },
+      {
+        name: "Strawberry Croissant",
+        price: 4,
+        description: "Buttery croissant filled with sweet, fresh strawberries",
+        image: "strawberry_croissant.jpg",
+      },
+      {
+        name: "Almond Croissant",
+        price: 4.5,
+        description:
+          "Flaky croissant with almond cream filling & sliced almonds",
+        image: "almond_croissant.jpg",
+        vegan: true,
+      },
+      {
+        name: "Muffin",
+        price: 2,
+        description: "Soft baked chocolate chip muffin",
+        image: "muffin.jpg",
+        popular: true,
       },
       {
         name: "Blueberry Muffin",
@@ -104,10 +92,24 @@ const menuData = {
       },
       {
         name: "Brownie",
-        price: 2.5,
+        price: 3.5,
         description: "Rich & fudgy chocolate brownie",
         image: "brownie.jpg",
         new: true,
+      },
+      {
+        name: "Cinnamon Roll",
+        price: 4,
+        description: "Sweet roll filled with cinnamon & cream cheese icing",
+        image: "cinnamon_roll.jpg",
+      },
+      {
+        name: "Cheesecake Slice",
+        price: 4,
+        description:
+          "Rich, classic New York-style, topped with strawberry purée",
+        image: "cheesecake.jpg",
+        popular: true,
       },
     ],
   },
@@ -118,35 +120,38 @@ const menuData = {
       {
         name: "Club Sandwich",
         price: 6.5,
-        description: "Triple-layered sandwich with turkey, bacon, & lettuce",
+        description:
+          "Triple-layered sandwich with turkey, crispy bacon, lettuce & tomato",
         image: "club_sandwich.jpg",
         popular: true,
       },
       {
         name: "Grilled Cheese",
         price: 4,
-        description: "Golden grilled bread with melted cheese",
+        description:
+          "Golden grilled bread with a sharp cheddar & povolone blend",
         image: "grilled_cheese.jpg",
       },
       {
-        name: "Classic Caprese Sandwich",
+        name: "Classic Caprese Panini",
         price: 7,
         description:
-          "Fresh mozzarella, tomato, basil pesto, grilled chicken, & balsamic glaze on toasted artisan bread",
+          "Fresh mozzarella, sliced tomato, basil pesto, & balsamic glaze",
         image: "classic_capress_sandwich.jpg",
+        vegan: true,
       },
       {
         name: "Vegetarian Burger",
-        price: 4.5,
-        description: "Juicy & flavorful vegetarian patty",
+        price: 5,
+        description: "Hearty black bean patty with fresh cilantro & lime aioli",
         image: "vegetarian_burger.jpg",
         vegan: true,
         new: true,
       },
       {
-        name: "Bagel",
-        price: 3,
-        description: "Soft bagel sandwich with eggs, onion, & lettuce",
+        name: "Breakfast Bagel",
+        price: 5,
+        description: "Toasted everything bagel with egg, cheese, & arugula",
         image: "bagel.jpg",
         new: true,
       },
@@ -159,7 +164,7 @@ const menuData = {
       {
         name: "Iced Tea",
         price: 2.5,
-        description: "Refreshing chilled tea with lemon",
+        description: "Refreshing chilled black tea with lemon",
         image: "iced_tea.jpg",
         popular: true,
         new: true,
@@ -167,20 +172,20 @@ const menuData = {
       {
         name: "Lemonade",
         price: 3,
-        description: "Freshly squeezed lemon juice with sugar & mint",
+        description: "Freshly squeezed lemon juice with a touch of mint",
         image: "lemonade.jpg",
       },
       {
         name: "Smoothie",
         price: 5,
-        description: "Blend of seasonal fruits & yogurt",
+        description: "Blend of mixed berries, banana, & almond milk",
         image: "smoothie.jpg",
         lactoseFree: true,
       },
       {
         name: "Hot Chocolate",
         price: 3.5,
-        description: "Creamy cocoa drink with added milk",
+        description: "Warm & creamy cocoa drink",
         image: "hot_chocolate.jpg",
       },
     ],
@@ -191,20 +196,22 @@ const menuData = {
     items: [
       {
         name: "Pancakes",
-        price: 5,
-        description: "Stack of fluffy pancakes with maple syrup & berries",
+        price: 6,
+        description:
+          "Stack of fluffy pancakes topped with maple syrup & fresh berries",
         image: "pancakes.jpg",
       },
       {
-        name: "Omelette",
-        price: 4.5,
-        description: "Three-egg omelette with cheese & herbs",
+        name: "Three-Egg Omelette",
+        price: 6.5,
+        description:
+          "Omelette with cheddar, fresh herbs, served with sourdough toast",
         image: "omelette.jpg",
       },
       {
         name: "French Toast",
-        price: 5,
-        description: "Golden bread slices soaked in eggs & milk",
+        price: 5.5,
+        description: "Golden bread slices with powdered sugar & maple syrup",
         image: "french_toast.jpg",
         new: true,
       },
@@ -217,19 +224,22 @@ const menuData = {
       {
         name: "Chocolate Cake",
         price: 4.5,
-        description: "Rich & moist chocolate sponge cake",
+        description:
+          "Rich, moist chocolate sponge cake with dark ganache frosting",
         image: "chocolate_cake.jpg",
       },
       {
         name: "Ice Cream Sundae",
         price: 3,
-        description: "Vanilla ice cream with chocolate syrup & nuts",
+        description:
+          "Vanilla ice cream with chocolate syrup, nuts, & a cherry on top",
         image: "ice_cream_sundae.jpg",
       },
       {
         name: "Apple Pie Slice",
         price: 3,
-        description: "Classic pie with cinnamon-spiced apple filling",
+        description:
+          "Classic flaky crust pie with cinnamon-spiced apple filling",
         image: "apple_pie_slice.jpg",
         new: true,
         popular: true,
