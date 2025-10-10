ce97bd1c902404879c1ed2260dbf2148795caba1
chko0 <chadi.kouzayha@gmail.com>
2025-10-10 11:49:18 +0300

    perf: Resolve render-blocking CSS and improve LCP
    - Installs and configures 'vite-plugin-beasties' to generate and inline Critical CSS for the above-the-fold content, significantly reducing the Largest Contentful Paint (LCP) time

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,19 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import svgr from "@svgr/rollup";
+import { beasties } from "vite-plugin-beasties";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    svgr(),
+    beasties({
+      options: {
+        preload: "swap",
+        pruneSource: false,
+      },
+    }),
+  ],
 });
